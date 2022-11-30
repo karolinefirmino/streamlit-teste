4dc2bf81bcf99508762f9601b665fc803a205cdd
Karoline Rodrigues Firmino <57333936+karolinefirmino@users.noreply.github.com>
2022-11-30 14:26:28 -0300

    Add files via upload

--- crud2.py
+++ crud2.py
@@ -7,28 +7,13 @@ with st.form('Formulário de conta'):
     a, b = st.columns(2)
     inst = st.selectbox('Instrumento', ('Clarinete', 'Flauta Transversal'))
 
-    def format_func(inst):
+    def aluno(inst):
         if inst == 'Clarinete':
-            return clarinet_students[option]
-            option = st.selectbox("Select option", options=list(
-                clarinet_students.keys()), format_func=format_func)
-            #display = (clarinet_students.values())
-            #options = list(range(len(display)))
-            #value = st.selectbox("aluna/o", options, format_func=format_func)
-            # return value
+            student = st.selectbox(clarinet_students)
+            return student
         else:
-            return flute_students[option]
-            option = st.selectbox("Select option", options=list(
-                flute_students.keys()), format_func=format_func)
-            #display = (flute_students.values())
-            #options = list(range(len(display)))
-            # value = st.selectbox(
-            # "aluna/o", option, format_func=format_func)
-            # return value
-        st.write(f"You selected option {option} called {format_func(option)}")
-        print(type(option))
-    # format_func(option)
-    format_func(inst)
+            student = st.selectbox(flute_students)
+            return student
 
     a.number_input('Postura:')
     b.number_input('Ritmo:')
