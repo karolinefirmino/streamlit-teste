915cdfef35243e427af499b47c6db5061cdb4cde
Karoline Rodrigues Firmino <57333936+karolinefirmino@users.noreply.github.com>
2022-11-30 12:12:39 -0300

    Add files via upload

--- crud2.py
+++ crud2.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 
 with st.form('Formulário de conta'):
-    clarinet_students = {"1": 'Angela Pommer', "2": 'Gabriel', "3": 'Ana'}
-    flute_students = {"1": 'Vinicius', "2": 'Matheus'}
+    clarinet_students = {1: 'Angela Pommer', 2: 'Gabriel', 3: 'Ana'}
+    flute_students = {1: 'Vinicius', 2: 'Matheus'}
     a, b = st.columns(2)
     inst = st.selectbox('Instrumento', ('Clarinete', 'Flauta Transversal'))
     option = ""
