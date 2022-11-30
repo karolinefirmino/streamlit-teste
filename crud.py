39f965417795b4e9e26bb291c2c250322d2b063e
Karoline Rodrigues Firmino <57333936+karolinefirmino@users.noreply.github.com>
2022-11-30 10:29:25 -0300

    Add files via upload

--- crud.py
+++ crud.py
@@ -1,21 +1,20 @@
 import streamlit as st
 
 
-def inputs():
-    d = {}
+d = {}
 
-    with st.form(key="include_note"):
-        instruments_list = ['Clarinete', 'Flauta Transversal', 'Oboé']
-        input_instrument = st.text_selectbox(
-            "Instrumento", instruments_list)
-        if input_instrument == 'Clarinete':
-            list_clarinet_students = ['Angela Pommer', 'Fernando Grande', 'Ana Carolina',
-                                      'Ellen', 'Karoline Rodrigues Firmino', 'Ana Luísa Dias', 'Gabriel Abner', 'Paloma']
-        elif input_instrument == 'Flauta Tranversal':
-            list_flute_students = ['Adão', 'Carlos',
-                                   'Matheus', 'Paloma', 'Vinicius']
-        else:
-            list_ob_students = ['Lucas', 'Luís', 'Victória']
+with st.form(key="include_note"):
+    instruments_list = ['Clarinete', 'Flauta Transversal', 'Oboé']
+    input_instrument = st.text_selectbox(
+        "Instrumento", instruments_list)
+    if input_instrument == 'Clarinete':
+        list_clarinet_students = ['Angela Pommer', 'Fernando Grande', 'Ana Carolina',
+                                  'Ellen', 'Karoline Rodrigues Firmino', 'Ana Luísa Dias', 'Gabriel Abner', 'Paloma']
+    elif input_instrument == 'Flauta Tranversal':
+        list_flute_students = ['Adão', 'Carlos',
+                               'Matheus', 'Paloma', 'Vinicius']
+    else:
+        list_ob_students = ['Lucas', 'Luís', 'Victória']
         pieces_for_evalutation = st.text_input(
             'Peças executadas pelas/pelos estudantes')
         nota_postura = st.number_input('Postura')
@@ -25,6 +24,5 @@ def inputs():
         soma = nota_postura + nota_ritmo + nota_sonoridade + nota_interpretacao
         media = soma / range(soma)
         salvar = st.form_submit_button('Salvar Avaliação')
-
-    if d:
-        st.json(d)
+        if d:
+            st.json(d)
