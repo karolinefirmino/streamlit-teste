6df393755db53c753c5060c2ccc6b83dcf6374e4
Karoline Rodrigues Firmino <57333936+karolinefirmino@users.noreply.github.com>
2022-11-30 12:43:26 -0300

    Add files via upload

--- crud2.py
+++ crud2.py
@@ -6,18 +6,27 @@ with st.form('Formulário de conta'):
     flute_students = {1: 'Vinicius', 2: 'Matheus'}
     a, b = st.columns(2)
     inst = st.selectbox('Instrumento', ('Clarinete', 'Flauta Transversal'))
-    option = inst
 
     def format_func(option):
         if inst == 'Clarinete':
-            return clarinet_students[option]
-            option = st.selectbox("Select option", options=list(
-                clarinet_students.keys()), format_func=format_func=lambda x: clarinet_students[x])
+            # return clarinet_students[option]
+            # option = st.selectbox("Select option", options=list(
+            # clarinet_students.keys()), format_func=format_func)
+            display = (clarinet_students.values())
+            options = list(range(len(display)))
+            value = st.selectbox("aluna/o", options,
+                                 format_func=lambda x: display[x])
+            return value
         else:
-            return flute_students[option]
-            option = st.selectbox("Select option", options=list(
-                flute_students.keys()), format_func=format_func=lambda x: flute_students[x])
-    format_func(option)
+            # return flute_students[option]
+            # option = st.selectbox("Select option", options=list(
+            # flute_students.keys()), format_func=format_func)
+            display = (flute_students.values())
+            options = list(range(len(display)))
+            value = st.selectbox("aluna/o", options,
+                                 format_func=lambda x: display[x])
+            return value
+    format_func()
 
     st.write(f"You selected option {option} called {format_func(option)}")
     a.number_input('Postura:')
