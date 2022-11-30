8f7efcdd8ebbf93df0aac9add6973b24dc085c65
Karoline Rodrigues Firmino <57333936+karolinefirmino@users.noreply.github.com>
2022-11-30 12:19:10 -0300

    Add files via upload

--- crud2.py
+++ crud2.py
@@ -13,11 +13,11 @@ with st.form('Formulário de conta'):
         if inst == 'Clarinete':
             return clarinet_students[option]
             option = st.selectbox("Select option", options=list(
-                clarinet_students.values()), format_func=lambda x: "option " + str(x))
+                clarinet_students.keys()), format_func=format_func)
         else:
             return flute_students[option]
             option = st.selectbox("Select option", options=list(
-                flute_students.values()), format_func=lambda x: "option " + str(x))
+                flute_students.keys()), format_func=format_func)
 
     st.write(f"You selected option {option} called {format_func(option)}")
     a.number_input('Postura:')
