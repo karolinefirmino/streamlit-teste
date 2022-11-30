74898f4a1959a84463760acffd09611df70bf066
Karoline Rodrigues Firmino <57333936+karolinefirmino@users.noreply.github.com>
2022-11-30 10:34:48 -0300

    Add files via upload

--- crud.py
+++ crud.py
@@ -3,26 +3,19 @@ import streamlit as st
 
 d = {}
 
-with st.form(key="include_note"):
-    instruments_list = ['Clarinete', 'Flauta Transversal', 'Oboé']
-    input_instrument = st.text_selectbox(
-        "Instrumento", instruments_list)
-    if input_instrument == 'Clarinete':
-        list_clarinet_students = ['Angela Pommer', 'Fernando Grande', 'Ana Carolina',
-                                  'Ellen', 'Karoline Rodrigues Firmino', 'Ana Luísa Dias', 'Gabriel Abner', 'Paloma']
-    elif input_instrument == 'Flauta Tranversal':
-        list_flute_students = ['Adão', 'Carlos',
-                               'Matheus', 'Paloma', 'Vinicius']
-    else:
-        list_ob_students = ['Lucas', 'Luís', 'Victória']
-        pieces_for_evalutation = st.text_input(
-            'Peças executadas pelas/pelos estudantes')
-        nota_postura = st.number_input('Postura')
-        nota_ritmo = st.number_input('Ritmo')
-        nota_sonoridade = st.number_input('Sonoridade')
-        nota_interpretacao = st.number_input('Interpretação')
-        soma = nota_postura + nota_ritmo + nota_sonoridade + nota_interpretacao
-        media = soma / range(soma)
-        salvar = st.form_submit_button('Salvar Avaliação')
-        if d:
-            st.json(d)
+with st.form('Formulário de conta'):
+    a, b = st.columns(2)
+    d['nome'] = a.text_input('Nome do Aluno:')
+    d['instrumento'] = b.text_selectbox(
+        'Instrumento', ('Flauta Transversal', 'Clarinete'))
+    d['nota_postura'] = st.number_input('Postura:')
+    d['nota_ritmo'] = st.number_input('Ritmo:')
+    d['nota_sonoridade'] = st.number_input('Sonoridade:')
+    d['nota_interpretacao'] = st.number_input('Interpretação:')
+
+    d['comentário'] = st.text_area('Um comentário:')
+
+    st.form_submit_button('Enviar')
+
+if d:
+    st.json(d)
