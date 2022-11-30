e1ff29040cdade479b8632c536dc8a138733fa57
Karoline Rodrigues Firmino <57333936+karolinefirmino@users.noreply.github.com>
2022-11-30 12:10:10 -0300

    Add files via upload

--- crud2.py
+++ crud2.py
@@ -6,7 +6,7 @@ with st.form('Formulário de conta'):
     flute_students = {1: 'Vinicius', 2: 'Matheus'}
     a, b = st.columns(2)
     inst = st.selectbox('Instrumento', ('Clarinete', 'Flauta Transversal'))
-    x = {}
+    option = st.selectbox()
 
     def format_func(option):
 
